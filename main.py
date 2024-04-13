47a3872a1b29bb8c4550b5d224f97ade74bac38a
Murilaom <166919237+Murilaom@users.noreply.github.com>
2024-04-13 13:22:52 +0000

    feat:👌inicializar projeto financeiro em python

---
+++ main.py
@@ -0,0 +1,50 @@
+class Initialize():
+    def __init__(self):
+        self.__transactions = []
+    
+    def show_menu(self):
+        print(50 * '-')
+        print('Bem vindo ao controle financeiro')
+        print(50 * '-')
+
+        print('1 - Adicionar Transação')
+        print('2 - Visualizar Transações')
+        print('3 - Sair')
+
+    def choose_options(self):
+        option = input('\nEscolha uma das opções: ')
+
+        if option != '1' and option != '2' and option !='3':
+            print('/nOpção Invalida')
+
+        return option
+    
+    def to_add(self):
+        operation = input('Informe o tipo da operação: ')
+        value = input('Informe o Valor')
+        description = input('Informe a descrição')
+
+        self.__transactions.append(
+            (operation, value, description)
+        )
+
+    def to_view(self):
+        for transaction in self.__transactions:
+            print(f'Operation:{transaction[0]} - Value:{transaction[1]} - Description {transaction[2]}')
+
+    def to_go_out(self):
+        print('\nObrigado, volte sempre!')
+
+    
+    if __name__ == '__main__':
+        option = ''
+
+        while option != 3:
+            init = initialize()
+            init.show_menu()
+            option = init.choose_option
+
+            if option =='1':
+                init.to.add()
+            elif option =='2':
+                init.to.add()
